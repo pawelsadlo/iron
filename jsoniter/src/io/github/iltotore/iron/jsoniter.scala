37d9a13f7d22e63edcfa3961a180796d2509e1ea
Pawel Sadlo <psadlo@virtuslab.com>
2025-06-03 11:57:21 +0200

    fix anonymous classes warning

--- jsoniter/src/io/github/iltotore/iron/jsoniter.scala
+++ jsoniter/src/io/github/iltotore/iron/jsoniter.scala
@@ -10,11 +10,33 @@ object jsoniter:
    *
    * @param constraint the [[Constraint]] implementation to test the decoded value.
    */
-  inline given makeCodec[A, B](using inline constraint: Constraint[A, B], inline config: CodecMakerConfig = CodecMakerConfig): JsonValueCodec[A :| B] = new:
-    private val codec = JsonCodecMaker.make[A](config)
-    def decodeValue(in: JsonReader, default: A :| B): A :| B =
+  inline given makeCodec[A, B](using
+      inline constraint: Constraint[A, B],
+      inline config: CodecMakerConfig = CodecMakerConfig
+  ): JsonValueCodec[A :| B] =
+    IronJsonValueCodec(JsonCodecMaker.make[A](config), (x: A) => constraint.test(x), constraint.message)
+
+  // Introduced to resolve "New anonymous class definition will be duplicated at each inline site".
+  private final class IronJsonValueCodec[A, B] private (codec: JsonValueCodec[A], constraintTest: A => Boolean, constraintMessage: String)
+      extends JsonValueCodec[A :| B]:
+    override def decodeValue(in: JsonReader, default: A :| B): A :| B =
       val decoded = codec.decodeValue(in, default)
-      if constraint.test(decoded) then decoded.asInstanceOf[A :| B]
-      else in.decodeError(constraint.message)
-    def encodeValue(x: A :| B, out: JsonWriter): Unit = codec.encodeValue(x, out)
-    def nullValue: A :| B = null.asInstanceOf[A :| B]
+      if constraintTest(decoded) then decoded.asInstanceOf[A :| B]
+      else in.decodeError(constraintMessage)
+
+    override def encodeValue(x: A :| B, out: JsonWriter): Unit =
+      codec.encodeValue(x, out)
+
+    override def nullValue: A :| B = null.asInstanceOf[A :| B]
+
+  end IronJsonValueCodec
+
+  /**
+   * Internal API, should not be used in user code.
+   */
+  object IronJsonValueCodec:
+    /**
+     * Internal API, should not be used in user code.
+     */
+    def apply[A, B](codec: JsonValueCodec[A], constraintTest: A => Boolean, constraintMessage: String): JsonValueCodec[A :| B] =
+      new IronJsonValueCodec(codec, constraintTest, constraintMessage)
